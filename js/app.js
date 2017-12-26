a7de7122b11c3d5c461f8d2c197cb6089e3300c7
EstrellaBF <stern.von.nord>
2017-12-25 20:58:50 -0500

    jalando info de archivo data como jquery

--- js/app.js
+++ js/app.js
@@ -3,6 +3,8 @@ $(document).ready(function(){
   var $splash = $('#splash');  
   var $platform = $('#platform'); 
   var $map = $('#map');
+  var $searchRestaurant = $('#search-restaurant');
+  var $data = $('data');
 
 
   $platform.hide();
@@ -16,5 +18,31 @@ $(document).ready(function(){
   //condición si se hace click o no a la ventana splash
   $splash.on('click', showPlatform);
     //console.log('hizo click');
-     
+  
+  //Sacando data, sacando los distritos
+  var $districts = function  $(data){
+    var arrData = Object.keys(data);
+    return arrData;
+  };
+  $cosha = $districts(data)
+console.log($districts(data));
+console.log($cosha);
+$( "h3" ).text($districts(data));
+
+  //var $newData = districts(data);
+  //console.log($newData);
+  //
+  //$newData.each(function(){
+    //console.log();
+  //});
+
+  //console.log(districts(data)[0]); // 37, en un array menciona los distritos
+  
+  //Buscando restaurantes que coincidan
+  //if($searchRestaurant ===)
+  
+  
+
+  
+
 });
--- js/maps.js
+++ js/maps.js
@@ -1,7 +1,11 @@
 function initMap(){
     var location = {lat: -12.062523, lng: -77.097223};
     var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 4,
+        zoom: 5,
         center: location
     });
+    var marker = new google.maps.Marker({
+        position: location,
+        map: map
+      });
 }
