dc9be913b684bfb8cf9fd20a623770ce8339b653
EstrellaBF <stern.von.nord>
2017-12-29 02:06:25 -0500

    verificando si se hace click o no cambia de vista de pagina

--- js/intro.js
+++ js/intro.js
@@ -1,6 +1,15 @@
 $(document).ready(function() {
-    setTimeout(function() {
-      window.location.href = '../views/restaurant.html';
-    }, 1000);
-  
+  //Si se hace click
+  $('body').on('click', function(){
+    window.location.href = 'views/restaurant.html';
   });
+
+  //si no se hace click, igual se ejecutará la página restaurant
+  setTimeout(function() {
+     window.location.href = 'views/restaurant.html';
+  }, 5000);
+  /*
+
+  
+  */
+});
