77ac82f1c6178e6c471b9e7529ce95aaf1c21a9f
EstrellaBF <stern.von.nord>
2018-01-02 01:08:29 -0500

    actualizando data y modal

--- js/data.js
+++ js/data.js
@@ -6,15 +6,15 @@ var data = {
       },
       'Japanese Food at Beach Ventanilla': {
         'photo':'http://diamantico.es/wp-content/uploads/2015/08/图片29.png',
-        'opinion': 'good'
+        'opinion': 'bad'
       },
       'Vegetarian Food at Gambetta Road' : {
         'photo':'http://www.nihontei.co.jp/cms_nihontei/wp-content/uploads/2016/05/head12hamburg.png',
-        'opinion': 'good'
+        'opinion': 'excellent'
       },
       'Amazonian Food at Industrial Park':{
         'photo':'https://www.realworldholidays.co.uk/blog/wp-content/uploads/2015/10/amazon-food.jpg',
-        'opinion': 'good'
+        'opinion': 'terrible'
       },
       'Arabic Food at Central Road':{
         'photo':'http://www.crazymasalafood.com/wp-content/images/20-best-arabian-food-in-bangalore/savoury-restaurant.jpg',
@@ -28,11 +28,11 @@ var data = {
   'LA PUNTA': {
       'Italian Food at Naval School': {
         'photo':'http://www.foodiebytes.com/image/x160/t/chain_menu_item/aac1b947edbead2dd93e0f61e7837c1e.jpg',
-        'opinion': 'good'
+        'opinion': 'bad'
       },
       'Japanese Food at Club Regatas': {
         'photo':'http://diamantico.es/wp-content/uploads/2015/08/图片29.png',
-        'opinion': 'good'
+        'opinion': 'excellent'
       },
       'Vegetarian Food at the beach' : {
         'photo':'http://www.nihontei.co.jp/cms_nihontei/wp-content/uploads/2016/05/head12hamburg.png',
@@ -40,11 +40,11 @@ var data = {
       },
       'Amazonian Food at Nautic Club':{
         'photo':'https://www.realworldholidays.co.uk/blog/wp-content/uploads/2015/10/amazon-food.jpg',
-        'opinion': 'good'
+        'opinion': 'terrible'
       },
       'Arabic Food at Miguel Grau Ave.':{
         'photo':'http://www.crazymasalafood.com/wp-content/images/20-best-arabian-food-in-bangalore/savoury-restaurant.jpg',
-        'opinion': 'good'
+        'opinion': 'excellent'
       },
       'Cake Shop at La Punta Park':{
         'photo':'https://www.foodline.sg/PageImage/Caterer/The-Cake-Shop/6974_original.jpg',
@@ -54,11 +54,11 @@ var data = {
   'BELLAVISTA': {
       'Italian Food at Faucett Ave.': {
         'photo':'http://www.foodiebytes.com/image/x160/t/chain_menu_item/aac1b947edbead2dd93e0f61e7837c1e.jpg',
-        'opinion': 'good'
+        'opinion': 'bad'
       },
       'Japanese Food at Venezuela Ave.': {
         'photo':'http://diamantico.es/wp-content/uploads/2015/08/图片29.png',
-        'opinion': 'good'
+        'opinion': 'excellent'
       },
       'Vegetarian Food San Jose St.' : {
         'photo':'http://www.nihontei.co.jp/cms_nihontei/wp-content/uploads/2016/05/head12hamburg.png',
@@ -66,11 +66,11 @@ var data = {
       },
       'Amazonian Food at Mall Plaza':{
         'photo':'https://www.realworldholidays.co.uk/blog/wp-content/uploads/2015/10/amazon-food.jpg',
-        'opinion': 'good'
+        'opinion': 'bad'
       },
       'Arabic Food at Benavides Ave.':{
         'photo':'http://www.crazymasalafood.com/wp-content/images/20-best-arabian-food-in-bangalore/savoury-restaurant.jpg',
-        'opinion': 'good'
+        'opinion': 'excellent'
       },
       'Cake Shop at Faucett Ave.':{
         'photo':'https://www.foodline.sg/PageImage/Caterer/The-Cake-Shop/6974_original.jpg',
@@ -80,7 +80,7 @@ var data = {
   'SAN MARTIN DE PORRES': {
       'Italian Food at Perú Ave.': {
         'photo':'http://www.foodiebytes.com/image/x160/t/chain_menu_item/aac1b947edbead2dd93e0f61e7837c1e.jpg',
-        'opinion': 'good'
+        'opinion': 'bad'
       },
       'Japanese Food at Universitaria Ave.': {
         'photo':'http://diamantico.es/wp-content/uploads/2015/08/图片29.png',
@@ -88,11 +88,11 @@ var data = {
       },
       'Vegetarian Food at Tomás Valle Ave.' : {
         'photo':'http://www.nihontei.co.jp/cms_nihontei/wp-content/uploads/2016/05/head12hamburg.png',
-        'opinion': 'good'
+        'opinion': 'excellent'
       },
       'Amazonian Food at Izaguirre Ave.':{
         'photo':'https://www.realworldholidays.co.uk/blog/wp-content/uploads/2015/10/amazon-food.jpg',
-        'opinion': 'good'
+        'opinion': 'excellent'
       },
       'Arabic Food at Perú Ave.':{
         'photo':'http://www.crazymasalafood.com/wp-content/images/20-best-arabian-food-in-bangalore/savoury-restaurant.jpg',
@@ -106,7 +106,7 @@ var data = {
   'LOS OLIVOS': {
       'Italian Food at Universitaria Ave.': {
         'photo':'http://www.foodiebytes.com/image/x160/t/chain_menu_item/aac1b947edbead2dd93e0f61e7837c1e.jpg',
-        'opinion': 'good'
+        'opinion': 'excellent'
       },
       'Japanese Food at Naranjal Ave.': {
         'photo':'http://diamantico.es/wp-content/uploads/2015/08/图片29.png',
@@ -118,11 +118,11 @@ var data = {
       },
       'Amazonian Food at Huandoy Ave.':{
         'photo':'https://www.realworldholidays.co.uk/blog/wp-content/uploads/2015/10/amazon-food.jpg',
-        'opinion': 'good'
+        'opinion': 'excellent'
       },
       'Arabic Food at Las Palmeras Ave.':{
         'photo':'http://www.crazymasalafood.com/wp-content/images/20-best-arabian-food-in-bangalore/savoury-restaurant.jpg',
-        'opinion': 'good'
+        'opinion': 'bad'
       },
       'Cake Shop at Universitaria Ave.':{
         'photo':'https://www.foodline.sg/PageImage/Caterer/The-Cake-Shop/6974_original.jpg',
@@ -132,11 +132,11 @@ var data = {
   'CERCADO DE LIMA': {
       'Italian Food at Abancay Ave.': {
         'photo':'http://www.foodiebytes.com/image/x160/t/chain_menu_item/aac1b947edbead2dd93e0f61e7837c1e.jpg',
-        'opinion': 'good'
+        'opinion': 'excellent'
       },
       'Japanese Food at Tacna Ave.': {
         'photo':'http://diamantico.es/wp-content/uploads/2015/08/图片29.png',
-        'opinion': 'good'
+        'opinion': 'excellent'
       },
       'Vegetarian Food at Wilson Ave.' : {
         'photo':'http://www.nihontei.co.jp/cms_nihontei/wp-content/uploads/2016/05/head12hamburg.png',
@@ -144,7 +144,7 @@ var data = {
       },
       'Amazonian Food at Tacna Ave.':{
         'photo':'https://www.realworldholidays.co.uk/blog/wp-content/uploads/2015/10/amazon-food.jpg',
-        'opinion': 'good'
+        'opinion': 'bad'
       },
       'Arabic Food at Jiron de la Union':{
         'photo':'http://www.crazymasalafood.com/wp-content/images/20-best-arabian-food-in-bangalore/savoury-restaurant.jpg',
@@ -166,7 +166,7 @@ var data = {
       },
       'Vegetarian Food at Arica Ave.' : {
         'photo':'http://www.nihontei.co.jp/cms_nihontei/wp-content/uploads/2016/05/head12hamburg.png',
-        'opinion': 'good'
+        'opinion': 'excellent'
       },
       'Amazonian Food at Venezuela Ave.':{
         'photo':'https://www.realworldholidays.co.uk/blog/wp-content/uploads/2015/10/amazon-food.jpg',
@@ -174,7 +174,7 @@ var data = {
       },
       'Arabic Food at Bolivia Ave.':{
         'photo':'http://www.crazymasalafood.com/wp-content/images/20-best-arabian-food-in-bangalore/savoury-restaurant.jpg',
-        'opinion': 'good'
+        'opinion': 'excellent'
       },
       'Cake Shop at Aguarico Ave.':{
         'photo':'https://www.foodline.sg/PageImage/Caterer/The-Cake-Shop/6974_original.jpg',
@@ -188,7 +188,7 @@ var data = {
       },
       'Japanese Food at La Marina Ave.': {
         'photo':'http://diamantico.es/wp-content/uploads/2015/08/图片29.png',
-        'opinion': 'good'
+        'opinion': 'bad'
       },
       'Vegetarian Food at Faucett Ave.' : {
         'photo':'http://www.nihontei.co.jp/cms_nihontei/wp-content/uploads/2016/05/head12hamburg.png',
@@ -200,7 +200,7 @@ var data = {
       },
       'Arabic Food at Universitaria Ave.':{
         'photo':'http://www.crazymasalafood.com/wp-content/images/20-best-arabian-food-in-bangalore/savoury-restaurant.jpg',
-        'opinion': 'good'
+        'opinion': 'excellent'
       },
       'Cake Shop at Venezuela Ave.':{
         'photo':'https://www.foodline.sg/PageImage/Caterer/The-Cake-Shop/6974_original.jpg',
@@ -218,7 +218,7 @@ var data = {
       },
       'Vegetarian Food at Salaverry Ave.' : {
         'photo':'http://www.nihontei.co.jp/cms_nihontei/wp-content/uploads/2016/05/head12hamburg.png',
-        'opinion': 'good'
+        'opinion': 'excellent'
       },
       'Amazonian Food at San Felipe Ave.':{
         'photo':'https://www.realworldholidays.co.uk/blog/wp-content/uploads/2015/10/amazon-food.jpg',
@@ -226,7 +226,7 @@ var data = {
       },
       'Arabic Food at Brasil Ave.':{
         'photo':'http://www.crazymasalafood.com/wp-content/images/20-best-arabian-food-in-bangalore/savoury-restaurant.jpg',
-        'opinion': 'good'
+        'opinion': 'bad'
       },
       'Cake Shop at Brasil Ave.':{
         'photo':'https://www.foodline.sg/PageImage/Caterer/The-Cake-Shop/6974_original.jpg',
@@ -236,7 +236,7 @@ var data = {
   'LINCE': {
       'Italian Food At Arequipa Ave.': {
         'photo':'http://www.foodiebytes.com/image/x160/t/chain_menu_item/aac1b947edbead2dd93e0f61e7837c1e.jpg',
-        'opinion': 'good'
+        'opinion': 'excellent'
       },
       'Japanese Food at Canevaro Ave.': {
         'photo':'http://diamantico.es/wp-content/uploads/2015/08/图片29.png',
@@ -244,7 +244,7 @@ var data = {
       },
       'Vegetarian Food at Arenales Ave.' : {
         'photo':'http://www.nihontei.co.jp/cms_nihontei/wp-content/uploads/2016/05/head12hamburg.png',
-        'opinion': 'good'
+        'opinion': 'bad'
       },
       'Amazonian Food near to Castilla Park':{
         'photo':'https://www.realworldholidays.co.uk/blog/wp-content/uploads/2015/10/amazon-food.jpg',
@@ -266,7 +266,7 @@ var data = {
       },
       'Japanese Food at Javier Prado Ave.': {
         'photo':'http://diamantico.es/wp-content/uploads/2015/08/图片29.png',
-        'opinion': 'good'
+        'opinion': 'excellent'
       },
       'Vegetarian Food at Salaverry Ave.' : {
         'photo':'http://www.nihontei.co.jp/cms_nihontei/wp-content/uploads/2016/05/head12hamburg.png',
@@ -278,7 +278,7 @@ var data = {
       },
       'Arabic Food Del Ejercito Ave.':{
         'photo':'http://www.crazymasalafood.com/wp-content/images/20-best-arabian-food-in-bangalore/savoury-restaurant.jpg',
-        'opinion': 'good'
+        'opinion': 'bad'
       },
       'Cake Shop at Camino Real Ave.':{
         'photo':'https://www.foodline.sg/PageImage/Caterer/The-Cake-Shop/6974_original.jpg',
@@ -288,7 +288,7 @@ var data = {
   'MIRAFLORES': {
       'Italian Food at Berlin St.': {
         'photo':'http://www.foodiebytes.com/image/x160/t/chain_menu_item/aac1b947edbead2dd93e0f61e7837c1e.jpg',
-        'opinion': 'good'
+        'opinion': 'bad'
       },
       'Japanese Food at Schell St.': {
         'photo':'http://diamantico.es/wp-content/uploads/2015/08/图片29.png',
@@ -318,7 +318,7 @@ var data = {
       },
       'Japanese Food at El Sol Ave.': {
         'photo':'http://diamantico.es/wp-content/uploads/2015/08/图片29.png',
-        'opinion': 'good'
+        'opinion': 'excellent'
       },
       'Vegetarian Food at Bolognesi Ave.' : {
         'photo':'http://www.nihontei.co.jp/cms_nihontei/wp-content/uploads/2016/05/head12hamburg.png',
@@ -326,11 +326,11 @@ var data = {
       },
       'Amazonian Food at San Martín Ave.':{
         'photo':'https://www.realworldholidays.co.uk/blog/wp-content/uploads/2015/10/amazon-food.jpg',
-        'opinion': 'good'
+        'opinion': 'bad'
       },
       'Arabic Food near to Bulevar Station ':{
         'photo':'http://www.crazymasalafood.com/wp-content/images/20-best-arabian-food-in-bangalore/savoury-restaurant.jpg',
-        'opinion': 'good'
+        'opinion': 'bad'
       },
       'Cake Shop at Pedro de Osma Ave.':{
         'photo':'https://www.foodline.sg/PageImage/Caterer/The-Cake-Shop/6974_original.jpg',
@@ -340,11 +340,11 @@ var data = {
   'SAN BORJA': {
       'Italian Food at Del Aire Ave.': {
         'photo':'http://www.foodiebytes.com/image/x160/t/chain_menu_item/aac1b947edbead2dd93e0f61e7837c1e.jpg',
-        'opinion': 'good'
+        'opinion': 'bad'
       },
       'Japanese Food at San Luis Ave.': {
         'photo':'http://diamantico.es/wp-content/uploads/2015/08/图片29.png',
-        'opinion': 'good'
+        'opinion': 'bad'
       },
       'Vegetarian Food at Aviación Ave.' : {
         'photo':'http://www.nihontei.co.jp/cms_nihontei/wp-content/uploads/2016/05/head12hamburg.png',
@@ -356,7 +356,7 @@ var data = {
       },
       'Arabic Food near to San Borja Av.':{
         'photo':'http://www.crazymasalafood.com/wp-content/images/20-best-arabian-food-in-bangalore/savoury-restaurant.jpg',
-        'opinion': 'good'
+        'opinion': 'excellent'
       },
       'Cake Shop at Javier Prado Ave.':{
         'photo':'https://www.foodline.sg/PageImage/Caterer/The-Cake-Shop/6974_original.jpg',
@@ -374,7 +374,7 @@ var data = {
       },
       'Vegetarian Food near to INC' : {
         'photo':'http://www.nihontei.co.jp/cms_nihontei/wp-content/uploads/2016/05/head12hamburg.png',
-        'opinion': 'good'
+        'opinion': 'excellent'
       },
       'Amazonian Food at El sol Ave.':{
         'photo':'https://www.realworldholidays.co.uk/blog/wp-content/uploads/2015/10/amazon-food.jpg',
@@ -382,7 +382,7 @@ var data = {
       },
       'Arabic Food at Matellini Ave.':{
         'photo':'http://www.crazymasalafood.com/wp-content/images/20-best-arabian-food-in-bangalore/savoury-restaurant.jpg',
-        'opinion': 'good'
+        'opinion': 'bad'
       },
       'Cake Shop at Guardia Civil Ave.':{
         'photo':'https://www.foodline.sg/PageImage/Caterer/The-Cake-Shop/6974_original.jpg',
